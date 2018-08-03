26049085fa2a6650844033e35d1de8a8daf7e0bf
wangbin <1029657709@qq.com>
2018-08-03 22:53:51 +0800

    submitByZengZhuo

--- main/main.js
+++ main/main.js
@@ -2,17 +2,43 @@ module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
 // Write your code here
+最大值 = ${sequence.maxmum()},平均值 = ${sequence.average()},元素数量 = ${sequence.number()},
 `);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
-  }
+    this.arr = input,
+    this.minimum = minimum,
+    this.maxmum = maxmum,
+    this.average = average,
+    this.number = number
+  },
 
   minimum() {
     // Write your code here
+     let newArr = this.arr.sort(function(a,b){return a-b});
+     return newArr[0];
   }
 
   // Write your code here
+   maxmum() {
+    // Write your code here
+     let newArr = this.arr.sort(function(a,b){return a-b});
+     return newArr[this.arr.length-1];
+  }
+
+  average(){
+  	let sum = 0;
+    let i;
+    for(i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum/arr.length
+  }
+  number(){
+  	reutrn this.arr.length;
+  }
+
 }
